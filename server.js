6887294a405e01a13d4eedba0fe60cffecdd471e
Dean Sofer <proloser@hotmail.com>
2012-03-07 01:36:59 -0800

    Demo that stuff is working

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (learn) is running on Node.JS ' + process.version);
+  res.end('Hello Dean\nApp (learn) is running on Node.JS ' + process.version);
 }).listen(15391);
